5b88c31f7eb9627802e46aec3eeff62f66ee24f4
Dmytro <dsumcow@gmail.com>
2024-12-14 22:49:30 +0200

    Refactor DTOs mapping

--- Spotico.Server/Controllers/CustomerController.cs
+++ Spotico.Server/Controllers/CustomerController.cs
@@ -1,8 +1,10 @@
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
 using Spotico.Core.Database;
 using Spotico.Core.Models;
 using Spotico.Core.Stores;
 using Spotico.Persistence;
+using Spotico.Server.DTOs;
 
 namespace Spotico.Server.Controllers;
 
@@ -12,7 +14,7 @@ public class CustomerController : ControllerBase
 {
     private readonly IUserStore _userRepository;
     
-    public CustomerController(SpoticoDbContext db, UserRepository userRepository)
+    public CustomerController(SpoticoDbContext db, IUserStore userRepository)
     {
         _userRepository = userRepository;
     }
@@ -26,9 +28,10 @@ public class CustomerController : ControllerBase
     }
     
     [HttpPost]
-    public async Task Post(User user)
+    public async Task Post(UserDTO user)
     {
-        await _userRepository.Add(user);
+        var customer = user.Adapt<User>(); // Adapt is a Mapster method that maps properties from one object to another
+        await _userRepository.Add(customer);
     }
     
     [HttpDelete("{id}")]
---
+++ Spotico.Server/DTOs/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Spotico.Server.DTOs;
+
+public class UserDTO
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
