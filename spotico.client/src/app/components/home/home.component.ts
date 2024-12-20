5788f71f73154bb6c090c33a7e05659f9a6cfb10
Dmytro <dsumcow@gmail.com>
2024-12-20 04:13:13 +0200

    Improve layout

--- spotico.client/src/app/components/home/home.component.ts
+++ spotico.client/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, SlicePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    SlicePipe
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -16,5 +17,8 @@ export class HomeComponent {
     { title: 'Stairway to Heaven', artist: 'Led Zeppelin', cover: 'https://i1.sndcdn.com/artworks-000127380203-93pa4d-t1080x1080.jpg' },
     { title: 'Imagine', artist: 'John Lennon', cover: 'https://cdn-images.dzcdn.net/images/cover/2675a9277dfabb74c32b7a3b2c9b0170/0x1900-000000-80-0-0.jpg' },
     { title: 'Smells Like Teen Spirit', artist: 'Nirvana', cover: 'https://i.scdn.co/image/ab67616d0000b273e175a19e530c898d167d39bf' },
+    { title: '92', artist: 'Валентин Стрыкало', cover: 'https://images.genius.com/8dc46c831d67172c02afe05d921747dd.1000x1000x1.png' },
+    { title: '92', artist: 'Валентин Стрыкало', cover: 'https://images.genius.com/8dc46c831d67172c02afe05d921747dd.1000x1000x1.png' },
+    { title: '92', artist: 'Валентин Стрыкало', cover: 'https://images.genius.com/8dc46c831d67172c02afe05d921747dd.1000x1000x1.png' },
   ]
 }
